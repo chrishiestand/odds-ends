1d2854e50fdac256f9b1e52cf341d09d4fa2a75c
Chris Hiestand <chrishiestand@gmail.com>
2011-08-18 12:22:25 -0700

    *Improved behavior - only time correlating log entries are stored when a time correlation is asked for *Added column padding for real tabular output

--- apache-log-correlate.py
+++ apache-log-correlate.py
@@ -6,6 +6,8 @@ from operator import itemgetter
 from datetime import datetime, date, time, timedelta, tzinfo
 
 class ApacheLogLine:
+  
+  maxLengths = {'date_time' : 9, 'seconds' : 7, 'bytes' : 5, 'status' : 6} #these numbers are lengths of hard-coded column headers, used for column padding
 
   def __init__(self, RequestData):
     if RequestData['bytes'] == '-':
@@ -14,19 +16,21 @@ class ApacheLogLine:
       if not RequestData[digits].isdigit():
         raise AttributeError('Could not determine bytes for requestData: ' + str(RequestData))
 
-    self.remote_host = RequestData['remote_host']
-    self.log_name = RequestData['log_name']
-    self.user_name = RequestData['user_name']
-    self.date_time = datetime.strptime(RequestData['date_time'], "%d/%b/%Y:%H:%M:%S %z") #%z requires python3.2 *grumbles*, also silently truncates timezone - see timetuple()
-    self.date_time = self.date_time.replace(tzinfo=None)  #even though the timezone isn't set, you have to force it back to naive mode
+    self.remote_host   = RequestData['remote_host']
+    self.log_name      = RequestData['log_name']
+    self.user_name     = RequestData['user_name']
+    self.date_time     = datetime.strptime(RequestData['date_time'], "%d/%b/%Y:%H:%M:%S %z") #%z requires python3.2 *grumbles*, also silently truncates timezone - see timetuple()
+    self.date_time     = self.date_time.replace(tzinfo=None)  #even though the timezone isn't set, you have to force it back to naive mode
     self.request_line1 = RequestData['request_line1']
-    self.status = int(RequestData['status'])
-    self.bytes = int(RequestData['bytes'])
-    self.seconds = int(RequestData['seconds'])
-    self.milliseconds = int(RequestData['milliseconds'])
-    
+    self.status        = int(RequestData['status'])
+    self.bytes         = int(RequestData['bytes'])
+    self.seconds       = int(RequestData['seconds'])
+    self.milliseconds  = int(RequestData['milliseconds'])
+
+
   def print(self):
-    print(self.date_time.strftime("%Y-%m-%d:%H:%M:%S"), ' ', self.seconds , ' ' , self.bytes, ' ', self.status, ' ', self.request_line1)
+    print(self.date_time.strftime("%Y-%m-%d:%H:%M:%S").ljust(ApacheLogLine.maxLengths['date_time']), ' ', str(self.seconds).rjust(ApacheLogLine.maxLengths['seconds']) , ' ' , str(self.bytes).rjust(ApacheLogLine.maxLengths['bytes']), ' ', str(self.status).rjust(ApacheLogLine.maxLengths['status']), ' ', self.request_line1)
+
 
   def isDuring(self, targetTime):
     targetTime = datetime.strptime(targetTime, "%Y-%m-%d:%H:%M:%S")
@@ -62,16 +66,31 @@ for logfile in args.logs:
     try:
       matches = re.match("(?P<remote_host>\S+) (?P<log_name>\S+) (?P<user_name>\S+) \[(?P<date_time>[^\]]+)\] \"(?P<request_line1>.+)\" (?P<status>\d+) (?P<bytes>\S+) (?P<seconds>\d+) (?P<milliseconds>\d+) .*" , line.strip()).groupdict()
     except AttributeError: #skip non-matching lines
-      print("Skipping non-matching line: " + line)
+      print("Skipping non-conforming line: " + line)
       continue
       
     if matches and int(matches['seconds']) >= args.minimumResponseTime:
       try:
-        lines.append(ApacheLogLine(matches))
+        ALL = ApacheLogLine(matches)
+        
       except AttributeError:
         print("Could not parse line:\n" + line)
         raise
         sys.exit(3)
+
+    if args.targetTime:
+      if ALL.isDuring(args.targetTime):
+        lines.append(ALL)
+      else:
+        continue
+    else:
+      lines.append(ALL)
+
+    ApacheLogLine.maxLengths['date_time'] = max(ApacheLogLine.maxLengths['date_time'], len(str(ALL.date_time)))
+    ApacheLogLine.maxLengths['seconds']   = max(ApacheLogLine.maxLengths['seconds'], len(str(ALL.seconds)))
+    ApacheLogLine.maxLengths['bytes']     = max(ApacheLogLine.maxLengths['bytes'], len(str(ALL.bytes)))
+    ApacheLogLine.maxLengths['status']    = max(ApacheLogLine.maxLengths['status'], len(str(ALL.status)))
+
 
 if args.sort == 'date_time':
   lines = sorted(lines, key=lambda logline: logline.date_time)
@@ -80,10 +99,6 @@ elif args.sort == 'bytes':
 elif args.sort == 'seconds':
   lines = sorted(lines, key=lambda logline: logline.seconds)  
 
-
+print("Date-Time".ljust(ApacheLogLine.maxLengths['date_time']), ' ',   'Seconds'.rjust(ApacheLogLine.maxLengths['seconds']), ' ', 'Bytes'.rjust(ApacheLogLine.maxLengths['bytes']) , ' ', 'Status'.rjust(ApacheLogLine.maxLengths['status']), ' ' ,  'Reuqest-Line-1')
 for line in lines:
-  if args.targetTime:
-    if line.isDuring(args.targetTime):
-      line.print()
-  else:
-    line.print()
+  line.print()
